dccc25745a5cca480eacfb15427494b73d9f9efb
hayoung <hyng0115@gmail.com>
2022-12-11 18:44:30 +0900

    init: 프로젝트 생성

---
+++ src/test/java/com/example/smilegateauthserver/SmilegateAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smilegateauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmilegateAuthServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
