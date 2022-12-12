a87a2b6a362009cbc35903a957a5fe9cd02c8f1e
hayoung <hyng0115@gmail.com>
2022-12-12 16:46:16 +0900

    chore: 요청 dto 어노테이션 변경

--- src/main/java/com/example/smilegateauthserver/user/dto/RegisterRequest.java
+++ src/main/java/com/example/smilegateauthserver/user/dto/RegisterRequest.java
@@ -3,17 +3,16 @@ package com.example.smilegateauthserver.user.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
-
-@NoArgsConstructor
+@RequiredArgsConstructor
 @Getter
 public class RegisterRequest {
   @NotBlank(message = "이메일은 필수 값입니다.")
   @Size(max = 255, message = "이메일의 최대 길이는 255자입니다.")
-  private String email;
+  private final String email;
 
   @NotBlank(message = "비밀번호는 필수 값입니다.")
   @Size(max = 255, message = "비밀번호의 최대 길이는 255자입니다.")
-  private String password;
+  private final String password;
 }
