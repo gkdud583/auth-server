83a999b9957dd242c0dd4c050ad51a3735ec0b43
hayoung <hyng0115@gmail.com>
2022-12-25 21:10:01 +0900

    :recycle: 불필요한 코드 삭제

--- src/main/java/com/example/smilegateauthserver/common/config/WebConfig.java
+++ src/main/java/com/example/smilegateauthserver/common/config/WebConfig.java
@@ -1,18 +1,13 @@
 package com.example.smilegateauthserver.common.config;
 
-import com.example.smilegateauthserver.common.argumentResolver.LoginUserArgumentResolver;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
-
-  private final LoginUserArgumentResolver loginUserArgumentResolver;
 
   @Override
   public void addViewControllers(ViewControllerRegistry registry) {
@@ -22,12 +17,4 @@ public class WebConfig implements WebMvcConfigurer {
     registry.addViewController("/users/list").setViewName("/users/list");
 
   }
-
-  @Override
-  public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-    resolvers.add(loginUserArgumentResolver);
-    WebMvcConfigurer.super.addArgumentResolvers(resolvers);
-  }
-
-
 }
--- src/main/java/com/example/smilegateauthserver/user/controller/UserController.java
+++ src/main/java/com/example/smilegateauthserver/user/controller/UserController.java
@@ -2,9 +2,7 @@ package com.example.smilegateauthserver.user.controller;
 
 import static com.example.smilegateauthserver.user.exception.ExceptionMessage.NOTFOUND_REFRESH_TOKEN;
 
-import com.example.smilegateauthserver.common.annotation.LoginUser;
 import com.example.smilegateauthserver.common.util.CookieUtil;
-import com.example.smilegateauthserver.user.User;
 import com.example.smilegateauthserver.user.controller.dto.LoginRequest;
 import com.example.smilegateauthserver.user.controller.dto.LoginResponse;
 import com.example.smilegateauthserver.user.controller.dto.RefreshTokenResponse;
@@ -19,6 +17,7 @@ import jakarta.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -46,8 +45,8 @@ public class UserController {
   }
 
   @PostMapping("logout")
-  public void logout(@LoginUser User user) {
-    userService.logout(user.getId());
+  public void logout(@AuthenticationPrincipal Object userId) {
+    userService.logout(Long.valueOf(String.valueOf(userId)));
   }
 
   @PostMapping("token/refresh")
